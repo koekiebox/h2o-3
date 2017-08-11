e5be3ad7d0ad8f78d0b05d0e9b517cd0bce5a0ac
wendycwong <wendycwong@yahoo.com>
2017-08-10 21:01:17 -0700

    PUBDEV-4802: max_runtime_secs too small.  Make sure all algos return valid models even when max_runtime_secs is too short. PUBDEV-4802: max_runtime_secs too short.  Moved back the max_runtime_secs tests for PUBDEV-4702.

--- h2o-algos/src/main/java/hex/deeplearning/DeepLearning.java
+++ h2o-algos/src/main/java/hex/deeplearning/DeepLearning.java
@@ -479,6 +479,8 @@ public class DeepLearning extends ModelBuilder<DeepLearningModel,DeepLearningMod
         if (!_parms._quiet_mode) {
           Log.info("==============================================================================================================================================================================");
           if (stop_requested()) {
+            warn("_max_runtime_secs", "Deep Learning model training was interrupted.  " +
+                    "Increase _max_runtime_secs or set it to 0 to disable it.");
             Log.info("Deep Learning model training was interrupted.");
           } else {
             Log.info("Finished training the Deep Learning model.");
